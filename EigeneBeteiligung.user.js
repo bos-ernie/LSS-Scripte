15fc5edafcc1dba8a515b009ee9a7831ab720e8c
types140 <types140@fahr-zur-hoelle.org>
2019-12-23 12:33:09 +0100

    Create EigeneBeteiligung.user.js

---
+++ EigeneBeteiligung.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Eigene Beteiligung
+// @version      1.0.0
+// @author       Allure149
+// @include      *://leitstellenspiel.de/missions/*
+// @include      *://www.leitstellenspiel.de/missions/*
+// @include      *://missionchief.co.uk/missions/*
+// @include      *://www.missionchief.co.uk/missions/*
+// @include      *://missionchief.com/missions/*
+// @include      *://www.missionchief.com/missions/*
+// @grant        none
+// ==/UserScript==
+/* global $ */
+
+(function() {
+    'use strict';
+
+    let own_vor_ort = $("#mission_vehicle_at_mission").find(".btn-backalarm-ajax").length;
+    let own_anfahrt = $("#mission_vehicle_driving").find(".btn-backalarm-ajax").closest(".hidden-xs").length;
+
+    $("#amount_of_people").append(`<span class="amount_of_people_label">Eigene Fahrzeuge <span class="label label-default">${own_anfahrt + own_vor_ort}</span></span>`);
+})();
