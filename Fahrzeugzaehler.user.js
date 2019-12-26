218c967314f6275fefc1ebe428f31a90fc8bbea4
types140 <types140@fahr-zur-hoelle.org>
2019-12-26 01:07:51 +0100

    Update Fahrzeugzaehler.user.js

--- Fahrzeugzaehler.user.js
+++ Fahrzeugzaehler.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Fahrzeugzaehler
-// @version      2.0.1
+// @version      2.1.0
 // @author       Allure149
 // @description  Zaehlt die Fahrzeuge auf Anfahrt und vor Ort von jedem am Einsatz teilnehmenden Kameraden
 // @include      *://leitstellenspiel.de/missions/*
@@ -9,6 +9,7 @@
 // @include      *://www.missionchief.co.uk/missions/*
 // @include      *://missionchief.com/missions/*
 // @include      *://www.missionchief.com/missions/*
+// @updateURL    https://github.com/types140/LSS-Scripte/blob/master/Fahrzeugzaehler.user.js
 // @grant        none
 // ==/UserScript==
 /* global $ */
@@ -22,6 +23,21 @@ $(function() {
             #fzTable tr:nth-child(even) { background-color: dimgrey; }
         </style>
     `);
+
+    let textOwnCars, textCars, textPlayers;
+    if(I18n.locale == "de"){
+        textOwnCars = "Eigene Fahrzeuge";
+        textCars = "Fahrzeuge";
+        textPlayers = "Spieler";
+    } else if(I18n.locale == "en"){
+        textOwnCars = "Own vehicles";
+        textCars = "Vehicles";
+        textPlayers = "Players";
+    } else if(I18n.locale == "fj"){
+        textOwnCars = "Own vehicles";
+        textCars = "Vehicles";
+        textPlayers = "Players";
+    }
 
     function isInArray(str, arr){
         for(let i = 0; i < arr.length; i++){
@@ -51,7 +67,7 @@ $(function() {
 
         arrNames.sort(function(a, b) { return a.count < b.count; });
 
-        outputText = `<table id="fzTable" class="table table-dark mission_header_info table-condensed"><tr class="panel-heading"><td>Eigene Fahrzeuge</td><td>${ownVehiclesOnDrive + ownVehiclesAtMission}</td></tr>`;
+        outputText = `<table id="fzTable" class="table table-dark mission_header_info table-condensed"><tr class="panel-heading"><td>${textOwnCars}</td><td>${ownVehiclesOnDrive + ownVehiclesAtMission}</td></tr>`;
         for(let j = 0; j < arrNames.length; j++){
             outputText += `<tr><td>${arrNames[j].name}</td><td>${arrNames[j].count}</td></tr>`;
         };
@@ -67,8 +83,8 @@ $(function() {
     let allVehiclesAtMission = $("#mission_vehicle_at_mission >> tr[id^='vehicle_row_']").length;
     fzMain();
 
-    $("#amount_of_people").append(`<span id="fzCars">Fahrzeuge <span class="label label-default">${allVehiclesOnDrive + allVehiclesAtMission}</span></span>
-                                   <span id="fzPlayers">Spieler <span class="label" style="background-color: dimgrey;">${ownVehiclesOnDrive + ownVehiclesAtMission != 0 ? arrNames.length + 1 : arrNames.length}</span></span>`);
+    $("#amount_of_people").append(`<span id="fzCars">${textCars} <span class="label label-default">${allVehiclesOnDrive + allVehiclesAtMission}</span></span>
+                                   <span id="fzPlayers">${textPlayers} <span class="label" style="background-color: dimgrey;">${ownVehiclesOnDrive + ownVehiclesAtMission != 0 ? arrNames.length + 1 : arrNames.length}</span></span>`);
 
     $("#fzCars").hover(() => {
         fzMain();
