a909e0835fc1dba4effcbef1d5ae2c12940d7227
types140 <types140@fahr-zur-hoelle.org>
2020-01-17 18:06:39 +0100

    Update ELWCheck.user.js

--- ELWCheck.user.js
+++ ELWCheck.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ELW-Check
-// @version      1.0.0
+// @version      1.1.1
 // @author       Allure149 exklusiv fuer den Verband Bundesweiter KatSchutz (Bund)
 // @include      *://leitstellenspiel.de/missions/*
 // @include      *://www.leitstellenspiel.de/missions/*
@@ -11,34 +11,43 @@
 (function() {
     'use strict';
 
-    let sendText = "Regelverstoß von %NAMES%"; //frei waehlbar, aber %NAMES% sollte vorhanden sein
-    let autoInsert = false; //true = wird in Rueckmeldung eingefuegt, nicht mehr extra
+    var config = {
+        //frei waehlbar, aber %NAMES% sollte vorhanden sein
+        text: "Regelverstoß von %NAMES%",
 
-    let checkIds, vehicle_type_id, playerName, returnElwCheck, alreadyChecked;
-    let namesAgainstLaw = [];
+        //true = wird in Rueckmeldung eingefuegt, nicht mehr extra
+        inTextfield: false
+    };
 
-    function check_elws(){
-        $("tr[id^=vehicle_row]").each(function(){
-            checkIds = ["3","34","78"]; // ELW1, ELW2, AB-Einsatzleitung
-            vehicle_type_id = $(this).find("a").attr("vehicle_type_id");
-            playerName = $(this).find("small > a[href^='/profile/']").text();
+    function getVehicleTypeId($parent){
+        return $parent.find("a").attr("vehicle_type_id");
+    }
 
-            if(checkIds.indexOf(vehicle_type_id) > -1 && !namesAgainstLaw.includes("@" + playerName)) {
-                namesAgainstLaw.push("@" + playerName);
+    function getPlayerName($parent){
+        return $parent.find("small > a[href^='/profile/']").text();
+    }
+
+    function checkElws(){
+        let checkIds = ["3","34","78"]; // ELW1, ELW2, AB-Einsatzleitung
+        let namesAgainstLaw = [];
+        let playerName = "";
+
+        $("tr[id^=vehicle_row], .vehicle_driving_hidden").each(function(){
+            playerName = "@" + getPlayerName($(this));
+
+            if(checkIds.indexOf(getVehicleTypeId($(this))) > -1 && !namesAgainstLaw.includes(playerName)) {
+                namesAgainstLaw.push(playerName);
             }
         });
 
-        if(namesAgainstLaw.length > 0) return sendText.replace("%NAMES%", namesAgainstLaw.join(" "));
-        else return "Keine Regelwidrigkeiten!";
+        if(namesAgainstLaw.length > 0) return config.text.replace("%NAMES%", namesAgainstLaw.join(" "));
+        else $("#elw_check").toggleClass("alert-danger alert-success"); return "Keine Regelwidrigkeiten!";
     }
 
     $("body").on("click", "#elw_check", function(){
-        if(!alreadyChecked) alreadyChecked = true;
-        returnElwCheck = check_elws;
-
-        if(autoInsert) $("#mission_reply_content").val(returnElwCheck);
-        else $("#elw_check").text(returnElwCheck)
+        let returnElwCheck = checkElws;
+        config.inTextfield ? $("#mission_reply_content").val(returnElwCheck) : $("#elw_check").text(returnElwCheck);
     });
 
-    $("#h2_write_feedback").before("<div id='elw_check' class='alert alert-danger' style='float:right; padding: 2px 5px; margin:0'>ELW-Check</div>");
+    $("#h2_write_feedback").before(`<div id="elw_check" class="alert alert-danger" style="float:right; padding: 2px 5px; margin:0">ELW-Check</div>`);
 })();
