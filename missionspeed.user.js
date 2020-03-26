3844924dc78b963f4681dfc7ab348612d6a3b4a2
types140 <types140@fahr-zur-hoelle.org>
2020-03-26 22:35:17 +0100

    FIX: mission_speed not defined

--- missionspeed.user.js
+++ missionspeed.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MissionSpeed
-// @version      2.0.0
+// @version      2.0.1
 // @author       Allure149
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com|missionchief-japan\.com|jocdispecerat112\.com)\/.*$/
 // @updateURL    https://github.com/types140/LSS-Scripte/raw/master/missionspeed.user.js
@@ -43,19 +43,29 @@
 
         switch(clickedId){
             case "mspa": $.get('/missionSpeed?speed=6');
+                         mission_speed = 6;
                 break;
             case "msfb": $.get('/missionSpeed?speed=5');
+                         mission_speed = 5;
                 break;
             case "msb": $.get('/missionSpeed?speed=4');
+                         mission_speed = 4;
                 break;
             case "mspl": $.get('/missionSpeed?speed=0');
+                         mission_speed = 0;
                 break;
             case "msf": $.get('/missionSpeed?speed=1');
+                         mission_speed = 1;
                 break;
             case "msff": $.get('/missionSpeed?speed=2');
+                         mission_speed = 2;
                 break;
-            case "msvf": if(user_premium) $.get('/missionSpeed?speed=3');
-                         else return false;
+            case "msvf": if(user_premium) {
+                             $.get('/missionSpeed?speed=3');
+                             mission_speed = 3;
+                         } else {
+                             return false;
+                         }
                 break;
         }
 
