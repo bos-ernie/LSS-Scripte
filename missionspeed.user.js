24ac07f3ba5be647939cc99dfdca37885dffd163
types140 <types140@fahr-zur-hoelle.org>
2020-03-26 21:52:12 +0100

    premiumcheck

--- missionspeed.user.js
+++ missionspeed.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MissionSpeed
-// @version      1.0.0
+// @version      1.1.0
 // @author       Allure149
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com|missionchief-japan\.com|jocdispecerat112\.com)\/.*$/
 // @updateURL    https://github.com/types140/LSS-Scripte/raw/master/missionspeed.user.js
@@ -13,15 +13,14 @@
     'use strict';
 
     $('#search_input_field_missions').before(`<div id="missionSpeed" class="btn-group">
-        <a id="mspa" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-pause"></div></a>
-        <a id="msfb" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-fast-backward"></div></a>
-        <a id="msb" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-backward"></div></a>
-        <a id="mspl" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-play"></div></a>
-        <a id="msf" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-forward"></div></a>
-        <a id="msff" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-fast-forward"></div></a>
-        <a id="msvf" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-plane"></div></a>
-    </div>`);
-	
+			                                      <a id="mspa" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-pause"></div></a>
+                                                  <a id="msfb" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-fast-backward"></div></a>
+			                                      <a id="msb" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-backward"></div></a>
+			                                      <a id="mspl" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-play"></div></a>
+			                                      <a id="msf" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-forward"></div></a>
+			                                      <a id="msff" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-fast-forward"></div></a>
+			                                      <a id="msvf" class="btn btn-xs btn-success"><div class="glyphicon glyphicon-plane"></div></a>
+			                                  </div>`);
     function readPage(data){
         var speed = $('.alert-success', data).prev().attr('href').replace('/missionSpeed?speed=','');
 
@@ -71,7 +70,8 @@
                 break;
             case "msff": $.get('/missionSpeed?speed=2');
                 break;
-            case "msvf": $.get('/missionSpeed?speed=3');
+            case "msvf": if(user_premium) $.get('/missionSpeed?speed=3');
+                         else return false;
                 break;
         }
 
