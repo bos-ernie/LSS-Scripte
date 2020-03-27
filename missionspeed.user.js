e6f1efa3671c84c93c6d4ac005d9a15f851d8a59
types140 <types140@fahr-zur-hoelle.org>
2020-03-27 23:28:15 +0100

    remove pause button

--- missionspeed.user.js
+++ missionspeed.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MissionSpeed
-// @version      2.0.2
+// @version      2.1.0
 // @author       Allure149
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com|missionchief-japan\.com|jocdispecerat112\.com)\/.*$/
 // @updateURL    https://github.com/types140/LSS-Scripte/raw/master/missionspeed.user.js
@@ -39,6 +39,8 @@
                 break;
         }
     }
+
+    if($('#mission_speed_pause').length != 0) $('#mission_speed_pause').remove();
 
     $('#mspa, #msfb, #msb, #mspl, #msf, #msff, #msvf').on('click', function(){
         var clickedId = $(this).attr('id');
