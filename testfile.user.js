b6d388421c5c3d293a7ecb2c97100bb171413f77
types140 <types140@fahr-zur-hoelle.org>
2020-05-05 15:15:42 +0200

    Update testfile.user.js

--- testfile.user.js
+++ testfile.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Testscript
-// @version      1.0.0
+// @version      2.0.0
 // @author       Allure149
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com)\/.*$/
 // @grant        none
@@ -11,4 +11,5 @@
     'use strict';
 
     console.log(GM_info.script.version);
+    console.log("Version 2");
 })();
