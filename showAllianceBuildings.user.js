206be58e625e268a5bcd01957c2f93bb731232dc
types140 <types140@fahr-zur-hoelle.org>
2020-07-31 07:34:19 +0200

    Update showAllianceBuildings.user.js

--- showAllianceBuildings.user.js
+++ showAllianceBuildings.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name         Verbandsgebaeude einblenden
-// @version      1.0.0
+// @version      1.0.1
 // @description  Behebt den aktuellen Fehler Verbandsgebaeude nicht einblenden zu koennen
 // @author       Allure149
-// @include      *://www.leitstellenspiel.de/
-// @include      *://leitstellenspiel.de/
+// @include      /^https?:\/\/(?:w{3}\.)?(?:leitstellenspiel\.de|(?:meldkamerspel|missionchief|missionchief-australia|nodsentralspillet|112-merkez|jogo-operador112|operador193|dyspetcher101-game|missionchief-japan|jocdispecerat112|missionchief-korea|hatakeskuspeli|dispecerske-centrum)\.com|missionchief\.co\.uk|centro-de-mando\.es|operatorratunkowy\.pl|larmcentralen-spelet\.se|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|operacni-stredisko\.cz|centro-de-mando\.mx)\/$/
 // @grant        none
 // ==/UserScript==
 
