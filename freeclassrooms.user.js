051b71def3dc6a09e93ba06232639c6da70f918b
types140 <types140@fahr-zur-hoelle.org>
2021-06-17 12:29:19 +0200

    Update freeclassrooms.user.js

--- freeclassrooms.user.js
+++ freeclassrooms.user.js
@@ -1,41 +1,54 @@
-var freeSchools = [];
+// ==UserScript==
+// @name         FreeClassrooms
+// @description  Zeigt die Anzahl freie Schulungsraeume pro Schule in der Gebaeudeuebersicht der Hauptseite an
+// @version      1.0.0
+// @author       Allure149
+// @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de)\/$/
+// @updateURL    https://github.com/types140/LSS-Scripte/raw/master/freeclassrooms.user.js
+// @downloadURL  https://github.com/types140/LSS-Scripte/raw/master/freeclassrooms.user.js
+// ==/UserScript==
+/* global $ */
 
-$.getJSON("/api/buildings.json", function(data){
-  let d = data;
-  const includedBuildings = [1,3,7,8,10]; //alle Schulen
+(function() {
+    'use strict';
 
-  for(let i = 0; i < d.length; i++){
-    let currentBuilding = d[i];
-    let countExtensions = 1;
-    if(includedBuildings.includes(currentBuilding.building_type) && currentBuilding.schoolings){
-      //if(currentBuilding.enabled)
-      var countSchoolings = currentBuilding.schoolings.length;
-      
-      for(let j = 0; j < currentBuilding.extensions.length; j++){
-        let currentExtension = currentBuilding.extensions[j];
-        if(currentExtension.available) countExtensions++;
-      }
-      
-      freeSchools.push({"buildingId":currentBuilding.id,"freeRooms":countExtensions-countSchoolings});
-    }
-  }
+    var freeSchools = [];
 
-  for(let j = 0; j < freeSchools.length; j++){
-    let currentSchool = freeSchools[j];
+    $.getJSON("/api/buildings.json", function(data){
+        let d = data;
+        const includedBuildings = [1,3,8,10]; //alle Schulen
 
-    let getStateColor = function(schoolrooms){
-      switch(schoolrooms){
-        case 1:
-        case 2: return "warning";
-          break;
-        case 3:
-        case 4: return "success";
-          break;
-        default: return "danger";
-      }
-    }
+        for(let i = 0; i < d.length; i++){
+            let currentBuilding = d[i];
+            let countExtensions = 1;
+            if(includedBuildings.includes(currentBuilding.building_type) && currentBuilding.schoolings){
+                var countSchoolings = currentBuilding.schoolings.length;
 
-    $("#building_list_caption_"+currentSchool.buildingId)
-      .append(`<span class="badge progress-bar-${getStateColor(currentSchool.freeRooms)}" style="margin-left: 5px">${currentSchool.freeRooms}</span>`);
-  }
-});
+                for(let j = 0; j < currentBuilding.extensions.length; j++){
+                    let currentExtension = currentBuilding.extensions[j];
+                    if(currentExtension.available) countExtensions++;
+                }
+
+                freeSchools.push({"buildingId":currentBuilding.id,"freeRooms":countExtensions-countSchoolings});
+            }
+        }
+
+        for(let j = 0; j < freeSchools.length; j++){
+            let currentSchool = freeSchools[j];
+
+            let getStateColor = function(schoolrooms){
+                switch(schoolrooms){
+                    case 1:
+                    case 2: return "warning";
+                        break;
+                    case 3:
+                    case 4: return "success";
+                        break;
+                    default: return "danger";
+                }
+            }
+
+            $("#building_list_caption_"+currentSchool.buildingId).append(`<span class="badge progress-bar-${getStateColor(currentSchool.freeRooms)}" style="margin-left: 5px">${currentSchool.freeRooms}</span>`);
+        }
+    });
+})();
