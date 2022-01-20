8297d5956f4dbf4a26ab8e75be3f07d68e1306df
types140 <types140@fahr-zur-hoelle.org>
2022-01-20 12:01:25 +0100

    Update setallfhzstate.user.js

--- setallfhzstate.user.js
+++ setallfhzstate.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Status aller Fhz gleichzeitig setzen
 // @description  Setzt alle Fahrzeuge einer Wache auf Status 2 oder 6
-// @version      1.0.0
+// @version      1.0.1
 // @author       Allure149
 // @include      /^https?:\/\/(?:w{3}\.)?(?:(policie\.)?operacni-stredisko\.cz|(politi\.)?alarmcentral-spil\.dk|(polizei\.)?leitstellenspiel\.de|(?:(police\.)?missionchief-australia|(police\.)?missionchief|(poliisi\.)?hatakeskuspeli|missionchief-japan|missionchief-korea|(politiet\.)?nodsentralspillet|(politie\.)?meldkamerspel|operador193|(policia\.)?jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|(police\.)?missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|(police\.)?operateur112\.fr|(polizia\.)?operatore112\.it|(policja\.)?operatorratunkowy\.pl|dispetcher112\.ru|(polis\.)?larmcentralen-spelet\.se)\/buildings\/.*$/
 // @updateURL    https://github.com/types140/LSS-Scripte/raw/master/setallfhzstate.user.js
@@ -28,7 +28,7 @@
     function setfms(state){
         $("#vehicle_table tbody tr").each(function(){
             var $this = $(this);
-            var vehicleId = $("td:nth-child(2) a", $this).attr("href").replace("/vehicles/","");
+            var vehicleId = $("a[href^='/vehicles/']:first", $this).attr("href").replace("/vehicles/","");
 
             $.get("/vehicles/"+vehicleId+"/set_fms/"+state).done(function(){
                 $("span[class~=building_list_fms][vehicle_id="+vehicleId+"]").toggleClass("building_list_fms_6 building_list_fms_2").text(state=="2"?"2":"6");
