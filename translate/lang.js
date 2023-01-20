92acd067e207da9a91c3e746c72d8e44a011ab24
types140 <types140@fahr-zur-hoelle.org>
2023-01-20 11:23:17 +0100

    Create lang.js

---
+++ translate/lang.js
@@ -0,0 +1,117 @@
+[
+        {
+            code: "nl_NL",
+            url: "https://www.meldkamerspel.com/",
+            polUrl: "https://politie.meldkamerspel.com/",
+        },
+        {
+            code: "en_US",
+            url: "https://www.missionchief.com/",
+            polUrl: "https://police.missionchief.com/",
+        },
+        {
+            code: "en_GB",
+            url: "https://www.missionchief.co.uk/",
+            polUrl: "https://police.missionchief.co.uk/",
+        },
+        {
+            code: "es_ES",
+            url: "https://www.centro-de-mando.es/",
+            polUrl: "",
+        },
+        {
+            code: "en_AU",
+            url: "https://www.missionchief-australia.com/",
+            polUrl: "https://police.missionchief-australia.com/",
+        },
+        {
+            code: "pl_PL",
+            url: "https://www.operatorratunkowy.pl/",
+            polUrl: "https://policja.operatorratunkowy.pl/",
+        },
+        {
+            code: "sv_SE",
+            url: "https://www.larmcentralen-spelet.se/",
+            polUrl: "https://polis.larmcentralen-spelet.se/",
+        },
+        {
+            code: "it_IT",
+            url: "https://www.operatore112.it/",
+            polUrl: "https://polizia.operatore112.it/",
+        },
+        {
+            code: "fr_FR",
+            url: "https://www.operateur112.fr/",
+            polUrl: "https://police.operateur112.fr/",
+        },
+        {
+            code: "ru_RU",
+            url: "https://www.dispetcher112.ru/",
+            polUrl: "",
+        },
+        {
+            code: "da_DK",
+            url: "https://www.alarmcentral-spil.dk/",
+            polUrl: "https://politi.alarmcentral-spil.dk/",
+        },
+        {
+            code: "nb_NO",
+            url: "https://www.nodsentralspillet.com/",
+            polUrl: "https://politiet.nodsentralspillet.com/",
+        },
+        {
+            code: "cs_CZ",
+            url: "https://www.operacni-stredisko.cz/",
+            polUrl: "https://policie.operacni-stredisko.cz/",
+        },
+        {
+            code: "tr_TR",
+            url: "https://www.112-merkez.com/",
+            polUrl: "",
+        },
+        {
+            code: "pt_PT",
+            url: "https://www.jogo-operador112.com/",
+            polUrl: "https://policia.jogo-operador112.com/",
+        },
+        {
+            code: "pt_BR",
+            url: "https://www.operador193.com/",
+            polUrl: "",
+        },
+        {
+            code: "uk_UA",
+            url: "https://www.dyspetcher101-game.com/",
+            polUrl: "",
+        },
+        {
+            code: "ko_KR",
+            url: "https://www.missionchief-korea.com/",
+            polUrl: "",
+        },
+        {
+            code: "es_MX",
+            url: "https://www.centro-de-mando.mx/",
+            polUrl: "",
+        },
+        {
+            code: "ja_JP",
+            url: "https://www.missionchief-japan.com/",
+            polUrl: "",
+        },
+        {
+            code: "ro_RO",
+            url: "https://www.jocdispecerat112.com/",
+            polUrl: "",
+        },
+        {
+            code: "fi_FI",
+            url: "https://www.hatakeskuspeli.com/",
+            polUrl: "https://poliisi.hatakeskuspeli.com/",
+        },
+        {
+            code: "sk_SK",
+            url: "https://www.dispecerske-centrum.com/",
+            polUrl: "",
+        },
+    ];
